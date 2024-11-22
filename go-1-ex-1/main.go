7f5fa7655a5ed8ad24338e4fd2ca2ee16f97ae80
Arber Idrizaj <Arber_Idrizaj@sluz.ch>
2024-11-22 15:04:17 +0100

    aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Arber"
+	lastName := "Idrizaj"
+	dayOfBirth := 15
+	monthOfBirth := 2
+	yearOfBirth := 2007
+	numberOfSiblings := 2
+	heightInMeters := 1.78
+	zodiacSign := '\u2652'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
