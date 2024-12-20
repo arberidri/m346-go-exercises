f46b24e087aa72c324d7624404c2a89f32ddc937
arberidri <Arber_Idrizaj@sluz.ch>
2024-12-20 14:42:54 +0100

    exc 4-2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,20 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
 
+// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypothenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 func main() {
 	// TODO: call the function computeHypotenuse
+	a := 4.0
+	b := 3.0
+
+	hypotenuse := computeHypothenuse(a, b)
+	fmt.Printf("Die Hypotenuse des Dreiecks mit a=%.2f und b=%.2f ist c=%.2f\n", a, b, hypotenuse)
+
 }
