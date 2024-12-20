3121b8ae95bc0c3f427b6d173731a56e54ecb8b3
arberidri <Arber_Idrizaj@sluz.ch>
2024-12-20 14:30:30 +0100

    exc 4-1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(E, M float64) float64 {
+	grade := 1.0 + 5.0*(E/M)
+	if grade > 6.0 {
+		return 6.0
+	}
+	if grade < 1.0 {
+		return 1.0
+	}
+	return grade
+}
 
 func main() {
+	fmt.Println(computeGrade(17.5, 28.0))
+	fmt.Println(computeGrade(0, 28.0))
+	fmt.Println(computeGrade(28.0, 28.0))
+	fmt.Println(computeGrade(-28.0, 28.0))
 	// TODO: call the function computeGrade
 }
